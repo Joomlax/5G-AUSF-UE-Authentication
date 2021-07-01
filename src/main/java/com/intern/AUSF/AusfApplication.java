dbaddf6eb6e89fde0e7ea33a565e03ce22f36d62
Dogukan <dogukangulyasar@gmail.com>
2021-07-01 16:43:29 +0300

    Project init

---
+++ src/main/java/com/intern/AUSF/AusfApplication.java
@@ -0,0 +1,13 @@
+package com.intern.AUSF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AusfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AusfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intern/AUSF/AusfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intern.AUSF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AusfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
