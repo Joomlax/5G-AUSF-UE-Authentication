4e2b205611f63616cf47334a7d659f90adca2109
Dogukan <dogukangulyasar@gmail.com>
2021-07-01 17:06:25 +0300

    Edited constructors

--- src/main/java/com/intern/AUSF/ue/Auth.java
+++ src/main/java/com/intern/AUSF/ue/Auth.java
@@ -7,12 +7,16 @@ public class Auth {
     private String servingNetworkName; //Required
 
     private String pei;
+    private String rand;
+    private String auts;
     private String routingIndicator;
     private ArrayList<String> cellCagInfo;
     private boolean n5gcInd;
     private String supportedFeatures;
 
-    //Creating constructors
+    //Constructors
+
+
     public Auth(String supiOrSuci, String servingNetworkName) {
         this.supiOrSuci = supiOrSuci;
         this.servingNetworkName = servingNetworkName;
@@ -24,39 +28,62 @@ public class Auth {
         this.pei = pei;
     }
 
-    public Auth(String supiOrSuci, String servingNetworkName, String pei, String routingIndicator) {
+    public Auth(String supiOrSuci, String servingNetworkName, String pei, String rand) {
         this.supiOrSuci = supiOrSuci;
         this.servingNetworkName = servingNetworkName;
         this.pei = pei;
+        this.rand = rand;
+    }
+
+    public Auth(String supiOrSuci, String servingNetworkName, String pei, String rand, String auts) {
+        this.supiOrSuci = supiOrSuci;
+        this.servingNetworkName = servingNetworkName;
+        this.pei = pei;
+        this.rand = rand;
+        this.auts = auts;
+    }
+
+    public Auth(String supiOrSuci, String servingNetworkName, String pei, String rand, String auts, String routingIndicator) {
+        this.supiOrSuci = supiOrSuci;
+        this.servingNetworkName = servingNetworkName;
+        this.pei = pei;
+        this.rand = rand;
+        this.auts = auts;
         this.routingIndicator = routingIndicator;
     }
 
-    public Auth(String supiOrSuci, String servingNetworkName, String pei, String routingIndicator, ArrayList<String> cellCagInfo) {
+    public Auth(String supiOrSuci, String servingNetworkName, String pei, String rand, String auts, String routingIndicator, ArrayList<String> cellCagInfo) {
         this.supiOrSuci = supiOrSuci;
         this.servingNetworkName = servingNetworkName;
         this.pei = pei;
+        this.rand = rand;
+        this.auts = auts;
         this.routingIndicator = routingIndicator;
         this.cellCagInfo = cellCagInfo;
     }
 
-    public Auth(String supiOrSuci, String servingNetworkName, String pei, String routingIndicator, ArrayList<String> cellCagInfo, boolean n5gcInd) {
+    public Auth(String supiOrSuci, String servingNetworkName, String pei, String rand, String auts, String routingIndicator, ArrayList<String> cellCagInfo, boolean n5gcInd) {
         this.supiOrSuci = supiOrSuci;
         this.servingNetworkName = servingNetworkName;
         this.pei = pei;
+        this.rand = rand;
+        this.auts = auts;
         this.routingIndicator = routingIndicator;
         this.cellCagInfo = cellCagInfo;
         this.n5gcInd = n5gcInd;
     }
 
-    public Auth(String supiOrSuci, String servingNetworkName, String pei, String routingIndicator, ArrayList<String> cellCagInfo, boolean n5gcInd, String supportedFeatures) {
+    public Auth(String supiOrSuci, String servingNetworkName, String pei, String rand, String auts, String routingIndicator, ArrayList<String> cellCagInfo, boolean n5gcInd, String supportedFeatures) {
         this.supiOrSuci = supiOrSuci;
         this.servingNetworkName = servingNetworkName;
         this.pei = pei;
+        this.rand = rand;
+        this.auts = auts;
         this.routingIndicator = routingIndicator;
         this.cellCagInfo = cellCagInfo;
         this.n5gcInd = n5gcInd;
         this.supportedFeatures = supportedFeatures;
     }
 
-    // Created all possible constructors from here.
+    //Created all constructors
 }
